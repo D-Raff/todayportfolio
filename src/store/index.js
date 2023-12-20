bf1f27f8b810192d688f93f702661d0a8cb29276
D-Raff <damonraffels@gmail.com>
2023-12-20 15:04:54 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,45 @@
 import { createStore } from 'vuex'
+const detour1 = 'https://d-raff.github.io/VueJS-Portfolio-data/data/'
 
 export default createStore({
   state: {
+    // strings must be null
+    jobTitle: null,
+    about: null,
+    education: null,
+    skills: null,
+    testimonials: null,
+    projects: null
   },
   getters: {
   },
   mutations: {
+    // this will update the state
+    setjobTitle(state, value){
+      state.jobTitle = value
+    },
+    setabout(state, value){
+      state.about = value
+    },
+    seteducation(state, value){
+      state.education = value
+    },
+    setskills(state, value){
+      state.skills = value
+    },
+    settestimonials(state, value){
+      state.testimonials = value
+    },
+    setprojects(state, value){
+      state.projects = value
+    },
   },
   actions: {
+    async fetchjobTitle(context){
+      let res = await fetch(detour1)
+      let {jobTitle} = await res.json()
+      context.commit('setjobTitle', jobTitle)
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <H1>HOME</H1>
   </div>
+  <div class="row">
+    <p class="lead" v-for="">
+
+    </p>
+  </div>
 </template>
 
 <script>
@@ -10,6 +15,14 @@
 export default {
   name: 'HomeView',
   components: {
-  }
+  },
+  computed: {
+    jobTitle(){
+      return this.$store.state.jobTitle
+    }
+  },
+  mounted() {
+    this.$store.dispatch'fetchjobTitle'
+  },
 }
 </script>
